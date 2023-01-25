b8e51f5d118765bf9b0fe9b17c045bb7ece14409
Issay Yoshida <issaymk2@gmail.com>
2023-01-25 15:48:37 +0900

    Fix windows build

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -1,4 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-
+if(${PORT} MATCHES "^(angle).*")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
